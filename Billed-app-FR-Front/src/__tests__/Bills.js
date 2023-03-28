bcd72bf6498fd7d0d5f3637f0fd56dc503af12f4
Maxime Zinderstein <maximezinderstein@gmail.com>
2023-03-28 09:36:54 +0200

    début des tests Bills.js

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,12 +2,13 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom"
+import { fireEvent, screen, waitFor } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
-
+import Bills from "../containers/Bills.js";
+import mockStore from "../__mocks__/store.js"
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
@@ -37,38 +38,86 @@ describe("Given I am connected as an employee", () => {
     })
     describe("When I click on the eye Icon of a Bill", () => {
       test("Then the modal should open and show the right file", async () => {
-        document.body.innerHTML = BillsUI({ data: bills })
+        document.body.innerHTML = BillsUI({ data: bills });
+        const handleClickIconEye = jest.fn(new Bills({
+          document,
+          onNavigate: jest.fn(),
+          store: mockStore,
+          localStorage: localStorageMock
+        }).handleClickIconEye);
         const eyes = screen.getAllByTestId("icon-eye");
+        const firstEye = eyes[0];
         const modal = screen.getByTestId("modaleFile");
-        await Promise.all(
-          eyes.map(async (eye) => {
-            console.log("1");
-            eye.click();
-            await waitFor(() => screen.getByTestId("modal-img"));
-            const modalImg = screen.getByTestId("modal-img");
-            console.log("2");
-            expect(
-              modal.classList.contains("show") &&
-                modalImg.src === eye.getAttribute("data-bill-url")
-            ).toBe(true);
-          })
-        );
+        firstEye.addEventListener('click', handleClickIconEye(firstEye));
+        firstEye.click();
+        console.log(modal.className);
+        const modalImg = screen.getByTestId("modal-img");
+        console.log(modalImg.src);
+        expect(modal.classList.contains("show")).toBe(true);
       });
     });
-    // describe("When I click on the eye Icon of a Bill", () => {
-    //   test("Then the modal should open and show the right file", async () => {
-    //     const eyes = screen.getAllByTestId('icon-eye')
-    //     const modal = screen.getByTestId('modaleFile')
-    //     eyes.forEach( async (eye)=> {
-    //       console.log("1")
-    //       eye.click()
-    //       await waitFor(() => screen.getByTestId('modal-img'))
-    //       let modalImg = screen.getByTestId('modal-img')
-    //       console.log("2")
-    //       expect(modal.classList.contains('show') && modalImg.src === eye.getAttribute("data-bill-url"))
-    //     })
-    //   })
-    // })
+  });
+
+  describe("When I navigate to the Bills page", () => {
+    test("fetches bills from mock API GET", async () => {
+      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "employee@test.com" }));
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getByText("Mes notes de frais"))
+      expect(screen.getByTestId("btn-new-bill")).toBeTruthy()
+    })
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills");
+        Object.defineProperty(
+          window,
+          'localStorage',
+          { value: localStorageMock }
+        );
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee',
+          email: "employee@test.com"
+        }));
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.appendChild(root);
+        router();
+      })
+      test("fetches bills from an API and fails with 404 message error", async () => {
+
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 404"));
+            }
+          }
+        })
+        window.onNavigate(ROUTES_PATH.Bills);
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/);
+        expect(message).toBeTruthy();
+      })
+
+      test("fetches messages from an API and fails with 500 message error", async () => {
+
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 500"))
+            }
+          }
+        })
+
+        window.onNavigate(ROUTES_PATH.Bills)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/)
+        expect(message).toBeTruthy()
+      })
+    })
+
   })
-})
+});
 
--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -24,21 +24,17 @@ describe("Given I'm connected as an employee", () => {
   });
     describe("When I select a file to upload that is not .jpg .jpeg or .png", () => {
       test("Then the form should be empty", () => {
-        console.log(newBill);
         const handleChangeFile = jest.fn(newBill.handleChangeFile);
         const fileInput = screen.getByTestId("file");
         fileInput.addEventListener("change", handleChangeFile);
         const file = new File(["test"], "test.pdf", { type: "application/pdf" });
         const event = { target: { files: [file] }}; 
-        console.log('fireEvent');
         fireEvent.change(fileInput, event);
-        // expect(handleChangeFile).toHaveBeenCalled();
         expect(handleChangeFile).toHaveReturnedWith(false);
       });
     });
     describe("When I select a file to upload that is .jpg .jpeg or .png", () => {
       test("Then the form should contain the file", () => {
-        console.log(newBill);
         const handleChangeFile = jest.fn(newBill.handleChangeFile);
         const fileInput = screen.getByTestId("file");
         fileInput.addEventListener("change", handleChangeFile);
@@ -49,75 +45,3 @@ describe("Given I'm connected as an employee", () => {
       });
     });
 });
-
-
-// describe("Given je suis un employee", () => {
-//   let newBill;
-
-//   beforeEach(() => {
-//     document.body.innerHTML = NewBillUI();
-//     newBill = new NewBill({
-//       document,
-//       onNavigate: jest.fn(),
-//       store: store,
-//       localStorage: localStorageMock,
-//     });
-//     console.log(newBill)
-//   });
-
-//   describe("Given je suis sur la page", () => {
-//     describe("When on passe une image", () => {
-//       test("Then bien", () => {
-//         const handleChangeFile = newBill.handleChangeFile;
-//         const input = screen.getByTestId("file");
-      //   const file = new File(["test"], "test.png", { type: "image/png" });
-      //   const event = { target: { files: [file] }, preventDefault: jest.fn() }; // Ajout de la méthode preventDefault()
-      //   fireEvent.change(input, event);
-      //   expect(input.files[0]).toStrictEqual(file);
-      // });
-//     });
-    
-
-//     describe("When on ne passe pas une image", () => {
-//       test("Then pas bien", () => {
-//         const handleChangeFile = newBill.handleChangeFile;
-//         const input = screen.getByTestId("file");
-//         const file = new File(["test"], "test.txt", { type: "text/plain" }); // Type de fichier incorrect
-//         const event = { target: { files: [file] }, preventDefault: jest.fn() }; // Ajout de la méthode preventDefault()
-//         fireEvent.change(input, event);
-//         expect(input.files[0].name).toBe(""); // Vérification que le nom du fichier est vide
-//       });
-//     });
-    
-//   });
-// });
-
-
-// describe("Given I am connected as an employee", () => {
-//   describe("When I am on NewBill Page", () => {
-//     describe('When I select a file to upload in the wrong format', () => {
-//       test('The input should be empty', () => {
-//         const newBill = new NewBill({
-//           document,
-//           onNavigate: jest.fn(),
-//           store: {},
-//           localStorage,
-//         });
-//         const spyHandleChangeFile = jest.spyOn(newBill, "handleChangeFile");
-
-//         const fileInput = screen.getByTestId("file");
-//         const file = new File(['file content'], 'file.pdf', { type: 'pdf' });
-//         console.log('file:', file);
-//         fireEvent.change(fileInput, {
-//         target: {
-//          files: [file],
-//         },
-//  });
-// console.log(fileInput.files[0].name);
-// console.log(fileInput.value);
-// expect(spyHandleChangeFile).toHaveBeenCalled();
-// expect(fileInput.files[0].name).toBe("");
-//       })
-//     })
-//   })
-// })
